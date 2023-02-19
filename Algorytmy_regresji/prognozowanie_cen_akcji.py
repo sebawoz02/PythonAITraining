046124636bdd0fc8b41f555b126d3cc0778638c5
sebawoz02 <268491@student.pwr.edu.pl>
2023-02-19 14:30:21 +0100

    prognozowanie_cen_akcji

---
+++ Algorytmy_regresji/prognozowanie_cen_akcji.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+
+"""Przeksztalcenie danych"""
+
+def add_original_feature(df, df_new):
+    df_new['open'] = df['Open']
+    df_new['open_1'] = df['Open'].shift(1)
+    df_new['close_1'] = df['Close'].shift(1)
+    df_new['high_1'] = df['High'].shift(1)
+    df_new['low_1'] = df['Low'].shift(1)
+    df_new['volume_1'] = df['Volume'].shift(1)
+
+
+def add_avg_price(df, df_new):
+    df_new['avg_price_5'] = df['Close'].rolling(5).mean().shift(1)
+    df_new['avg_price_30'] = df['Close'].rolling(21).mean().shift(1)
+    df_new['avg_price_365'] = df['Close'].rolling(252).mean().shift(1)
+    df_new['ratio_avg_price_5_30'] = df_new['avg_price_5'] / df_new['avg_price_30']
+    df_new['ratio_avg_price_5_365'] = df_new['avg_price_5'] / df_new['avg_price_365']
+    df_new['ratio_avg_price_30_365'] = df_new['avg_price_30'] / df_new['avg_price_365']
+
+
+def add_avg_volume(df, df_new):
+    df_new['avg_volume_5'] = df['Volume'].rolling(5).mean().shift(1)
+    df_new['avg_volume_30'] = df['Volume'].rolling(21).mean().shift(1)
+    df_new['avg_volume_365'] = df['Volume'].rolling(252).mean().shift(1)
+    df_new['ratio_avg_volume_5_30'] = df_new['avg_volume_5'] / df_new['avg_volume_30']
+    df_new['ratio_avg_volume_5_365'] = df_new['avg_volume_5'] / df_new['avg_volume_365']
+    df_new['ratio_avg_volume_30_365'] = df_new['avg_volume_30'] / df_new['avg_volume_365']
+
+
+def add_std_price(df, df_new):
+    df_new['std_price_5'] = df['Close'].rolling(5).std().shift(1)
+    df_new['std_price_30'] = df['Close'].rolling(21).std().shift(1)
+    df_new['std_price_365'] = df['Close'].rolling(252).std().shift(1)
+    df_new['ratio_std_price_5_30'] = df_new['std_price_5'] / df_new['std_price_30']
+    df_new['ratio_std_price_5_365'] = df_new['std_price_5'] / df_new['std_price_365']
+    df_new['ratio_std_price_30_365'] = df_new['std_price_30'] / df_new['std_price_365']
+
+
+def add_std_volume(df, df_new):
+    df_new['std_volume_5'] = df['Volume'].rolling(5).std().shift(1)
+    df_new['std_volume_30'] = df['Volume'].rolling(21).std().shift(1)
+    df_new['std_volume_365'] = df['Volume'].rolling(252).std().shift(1)
+    df_new['ratio_std_volume_5_30'] = df_new['std_volume_5'] / df_new['std_volume_30']
+    df_new['ratio_std_volume_5_365'] = df_new['std_volume_5'] / df_new['std_volume_365']
+    df_new['ratio_std_volume_30_365'] = df_new['std_volume_30'] / df_new['std_volume_365']
+
+
+def add_return_feature(df, df_new):
+    df_new['return_1'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
+    df_new['return_5'] = ((df['Close'] - df['Close'].shift(5)) / df['Close'].shift(5)).shift(1)
+    df_new['return_30'] = ((df['Close'] - df['Close'].shift(21)) / df['Close'].shift(21)).shift(1)
+    df_new['return_365'] = ((df['Close'] - df['Close'].shift(252)) / df['Close'].shift(252)).shift(1)
+    df_new['moving_avg_5'] = df_new['return_1'].rolling(5).mean().shift(1)
+    df_new['moving_avg_30'] = df_new['return_1'].rolling(21).mean().shift(1)
+    df_new['moving_avg_365'] = df_new['return_1'].rolling(252).mean().shift(1)
+
+
+def generate_features(df):
+    """
+    Funkcja generujaca cechy na podstawie histroycznych wartosci indeksu i jego zmiennosci
+    :param df: obiekt DataFrame zawierajacy kolumny 'Open', 'Close', 'High', 'Low', 'Volume', 'Adjusted Close'
+    :return: Obiekt DataFrame zawierajcy zbior danych z nowymi cechami
+    """
+    df_n = pd.DataFrame()
+    # 6 oryginalnych cech
+    add_original_feature(df, df_n)
+    # 31 wygenerowanych cech
+    add_avg_volume(df, df_n)
+    add_avg_price(df, df_n)
+    add_std_price(df, df_n)
+    add_std_volume(df, df_n)
+    add_return_feature(df, df_n)
+    df_n['close'] = df['Close']
+    df_n = df_n.dropna(axis=0)
+    return df_n
+
+
+raw_data = pd.read_csv("19880101_20230118.csv", index_col='Date')
+data = generate_features(raw_data)
+start_train = '1988-01-04'
+end_train = '2021-12-31'
+start_test = '2022-01-01'
+end_test = '2023-01-18'
+data_train = data.loc[start_train:end_train]
+X_train = data_train
